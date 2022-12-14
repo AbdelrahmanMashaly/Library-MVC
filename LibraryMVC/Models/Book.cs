2c73d55f6aea6e520f53259e78b95b6f53441a70
AbdelrahmanMashaly <abdelrahmanmashaly24@outlook.com>
2022-12-15 01:48:12 +0200

    Add project files.

---
+++ LibraryMVC/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryMVC.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; }
+
+    }
+}
---
+++ LibraryMVC/Models/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using LibraryMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryMVC.Models
+{
+    public class SeedData
+    {  
+        
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                // if the database empty then add these two books
+                // if it is not then return with the current books 
+                if (context.Book.Any())
+                {
+                    return;
+                }
+                context.Book.AddRange(new Book { Title = "Asp.net Developing", CallNumber = "ASQ 2000" },
+                    new Book { Title = "C sharp", CallNumber = "ASQ 2122" }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
